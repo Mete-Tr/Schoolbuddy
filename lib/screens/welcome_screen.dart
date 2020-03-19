d473fde1988e4df26716f31f69214ee9f86355e6
Mete Sagir <turan@metes-air.localdomain>
2020-03-19 02:46:46 +0100

    finished

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:x/database/moor_database.dart';
-import 'package:x/provider/profileProv.dart';
-import 'package:x/widgets/custom_listitem.dart';
-import '../models/subject.dart';
 import '../widgets/app_drawer.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -16,8 +13,8 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final dao = Provider.of<TimetableDao>(context);
-    List<Subject> data = [];
+    final timeDao = Provider.of<TimetableDao>(context);
+    final hmDao = Provider.of<HomeworkDataDao>(context);
 
     int today = DateTime.now().weekday;
 
@@ -33,100 +30,84 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         child: Column(
           children: <Widget>[
             Container(
-              height: 250,
+              height: 200,
               width: double.infinity,
               child: Card(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 elevation: 10,
-                child: FutureBuilder(
-                  future: Provider.of<ProfileProv>(context).getUserData(),
-                  builder: (context, snapshot) {
-                    SharedPreferences prefs = snapshot.data;
-                    if (snapshot?.hasData ?? false) {
-                      return Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Text(
-                          'Hallo, ' +
-                              prefs.getString('firstname') +
-                              ' ' +
-                              prefs.getString('lastname'),
-                          style: TextStyle(fontSize: 23),
-                        ),
-                      );
-                    } else {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
+                child: StreamBuilder(
+                  stream: hmDao.watchDoneHomeworks(),
+                  builder:
+                      (context, AsyncSnapshot<List<HomeworkData>> snapshot) {
+                    final list = snapshot.data ?? List();
+                    return Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Willkommen bei Schoolbuddy :)',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            'Du hast ${list.length} ungemachte Hausaufgaben.',
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ],
+                      ),
+                    );
                   },
                 ),
               ),
             ),
             Expanded(
               child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: StreamBuilder(
-                    stream: dao.watchTimetableDay(today == 1
-                        ? 'MO'
-                        : today == 2
-                            ? 'DI'
-                            : today == 3
-                                ? 'MIT'
-                                : today == 4
-                                    ? 'DO'
-                                    : today == 5 ? 'FRI' : 'MO'),
-                    builder:
-                        (context, AsyncSnapshot<List<Timetable>> snapshot) {
-                      final list = snapshot.data ?? List();
-                      return ListView.builder(
-                        itemCount: list.length,
-                        itemBuilder: (ctx, i) {
-                          return Card(
-                            color: Color(int.parse(list[i].color)),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: StreamBuilder(
+                  stream: timeDao.watchTimetableDay(today == 1
+                      ? 'MO'
+                      : today == 2
+                          ? 'DI'
+                          : today == 3
+                              ? 'MIT'
+                              : today == 4 ? 'DO' : today == 5 ? 'FRI' : 'MO'),
+                  builder: (context, AsyncSnapshot<List<Timetable>> snapshot) {
+                    final list = snapshot.data ?? List();
+                    return ListView.builder(
+                      itemCount: list.length,
+                      itemBuilder: (ctx, i) {
+                        return Card(
+                          color: Color(int.parse(list[i].color)),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          elevation: 8,
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            height: 80,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text('${i + 1}.',
+                                    style: TextStyle(fontSize: 26)),
+                                Text('${list[i].subjectAcronym}',
+                                    style: TextStyle(fontSize: 30)),
+                                Text('${list[i].room}',
+                                    style: TextStyle(fontSize: 26))
+                              ],
                             ),
-                            elevation: 8,
-                            child: Container(
-                              padding: EdgeInsets.all(5),
-                              height: 80,
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  Text('${i + 1}.',
-                                      style: TextStyle(fontSize: 26)),
-                                  Text('${list[i].subjectAcronym}',
-                                      style: TextStyle(fontSize: 30)),
-                                  Text('${list[i].room}',
-                                      style: TextStyle(fontSize: 26))
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  )
-                  //  data.isEmpty
-                  //     ? Center(
-                  //         child: Text(
-                  //           'Noch keine Daten vorhanden',
-                  //         ),
-                  //       )
-                  //     : ListView.builder(
-                  //         itemCount: data.length,
-                  //         itemBuilder: (BuildContext context, int i) {
-                  //           return Card(
-
-                  //               );
-                  //         },
-                  //       ),
-                  ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
             ),
           ],
         ),
